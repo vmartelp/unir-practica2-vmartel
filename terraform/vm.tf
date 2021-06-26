2eef28055acee9e854994a7deb29a3acff3fc476
vmartelp <83169926+vmartelp@users.noreply.github.com>
2021-06-26 11:27:26 +0100

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -88,3 +88,45 @@ resource "azurerm_linux_virtual_machine" "kubernetes-NFS" {
 
 ##########################################################################
 
+# Creamos una máquina virtual WORKER con Centos 8 Stream
+resource "azurerm_linux_virtual_machine" "kubernetes-worker" {
+    name                = "kubernetes-worker"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "adminUsername"
+    network_interface_ids = [ azurerm_network_interface.nicworker.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username   = "adminUsername"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.unirvictorstorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
