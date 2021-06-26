f5fe719e53a3b6fe439d1cfbd4a0b5d52e1da8df
vmartelp <83169926+vmartelp@users.noreply.github.com>
2021-06-26 11:22:34 +0100

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -40,3 +40,51 @@ resource "azurerm_linux_virtual_machine" "kubernetes-master" {
     }
 
 }
+
+########################################################################
+
+# Creamos una máquina virtual NFS con Centos 8 Stream
+resource "azurerm_linux_virtual_machine" "kubernetes-NFS" {
+    name                = "kubernetes-NFS"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "adminUsername"
+    network_interface_ids = [ azurerm_network_interface.nicnfs.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username   = "adminUsername"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.unirvictorstorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+##########################################################################
+
