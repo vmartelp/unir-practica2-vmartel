00ad8312dcca0fe30f7b3835b15259645a1b5107
vmartelp <83169926+vmartelp@users.noreply.github.com>
2021-06-26 11:17:34 +0100

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,6 +1,6 @@
-# Creamos un Security group
+# Creamos un Security group para el master
 
-resource "azurerm_network_security_group" "misecgroup" {
+resource "azurerm_network_security_group" "secgroupmaster" {
     name                = "sshtraffic"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
@@ -24,8 +24,42 @@ resource "azurerm_network_security_group" "misecgroup" {
 
 # Vinculamos el security group al interface de red del Master
 
-resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation1" {
+resource "azurerm_network_interface_security_group_association" "secgroupmaster" {
     network_interface_id      = azurerm_network_interface.nicmaster.id
-    network_security_group_id = azurerm_network_security_group.misecgroup.id
+    network_security_group_id = azurerm_network_security_group.secgroupmaster.id
+
+}
+
+#############################################################################################
+
+# Creamos un Security group para el NFS
+
+resource "azurerm_network_security_group" "secgroupnfs" {
+    name                = "sshtraffic"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+}
+
+# Vinculamos el security group al interface de red del NFS
+
+resource "azurerm_network_interface_security_group_association" "secgroupnfs" {
+    network_interface_id      = azurerm_network_interface.nicnfs.id
+    network_security_group_id = azurerm_network_security_group.secgroupnfs.id
 
 }
