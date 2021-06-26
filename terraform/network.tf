74a276aeda34236d04af0d1cc9c5bc87fd47f8f0
vmartelp <83169926+vmartelp@users.noreply.github.com>
2021-06-26 11:14:52 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -19,6 +19,8 @@ resource "azurerm_subnet" "misubnet" {
 
 }
 
+###################################################################
+
 #Creamos network interface card con ip estática para el master
 resource "azurerm_network_interface" "nicmaster" {
   name                = "nicmaster"  
@@ -39,10 +41,7 @@ resource "azurerm_network_interface" "nicmaster" {
 
 }
 
-
-
 #Creamos una IP publica para el Master
-
 resource "azurerm_public_ip" "ippublicamaster" {
   name                = "ippublicamaster"
   location            = azurerm_resource_group.rg.location
@@ -56,8 +55,39 @@ resource "azurerm_public_ip" "ippublicamaster" {
 
 }
 
+####################################################################
 
+#Creamos network interface card con ip estática para el NFS
+resource "azurerm_network_interface" "nicnfs" {
+  name                = "nicnfs"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
+    ip_configuration {
+    name                           = "myipconfiguration1"
+    subnet_id                      = azurerm_subnet.misubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.11"
+    public_ip_address_id           = azurerm_public_ip.ippublicanfs.id
+  }
 
+    tags = {
+        environment = "CP2"
+    }
 
+}
+
+#Creamos una IP publica para el NFS
+resource "azurerm_public_ip" "ippublicanfs" {
+  name                = "ippublicanfs"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
 
