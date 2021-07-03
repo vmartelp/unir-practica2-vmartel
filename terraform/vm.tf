cf51a439f35125211cde9c35ab6942e1346eda7f
vmartelp <83169926+vmartelp@users.noreply.github.com>
2021-07-03 13:01:25 +0100

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "kubernetes-master" {
     name                = "kubernetes-master"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
-    size                = var.vm_size
+    size                = var.vm_size_master
     admin_username      = "master"
     network_interface_ids = [ azurerm_network_interface.nicmaster.id ]
     disable_password_authentication = true
